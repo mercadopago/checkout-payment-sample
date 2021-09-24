8981fa9af90fd4db5ca559673dc506439ad2de21
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-09-24 13:57:32 -0300

    feat: update example to v2

--- server/node/server.js
+++ server/node/server.js
@@ -1,48 +1,57 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mercadopago = require("mercadopago");
 
-//REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
-mercadopago.configurations.setAccessToken("YOUR_ACCESS_TOKEN"); 
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "ACCESS_TOKEN",
+});
+  
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 app.use(express.static("../../client"));
 
 app.get("/", function (req, res) {
   res.status(200).sendFile("index.html");
 }); 
 
-app.post("/create_preference", (req, res) => {
+app.post("/preference", (req, res) => {
 
 	let preference = {
-		items: [{
-			title: req.body.description,
-			unit_price: Number(req.body.price),
-			quantity: Number(req.body.quantity),
-		}],
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
 		back_urls: {
 			"success": "http://localhost:8080/feedback",
 			"failure": "http://localhost:8080/feedback",
 			"pending": "http://localhost:8080/feedback"
 		},
-		auto_return: 'approved',
+		auto_return: "approved",
 	};
 
 	mercadopago.preferences.create(preference)
 		.then(function (response) {
-			res.json({id :response.body.id})
+			res.json({
+				id: response.body.id
+			});
 		}).catch(function (error) {
 			console.log(error);
 		});
 });
 
-app.get('/feedback', function(request, response) {
-	 response.json({
-		Payment: request.query.payment_id,
-		Status: request.query.status,
-		MerchantOrder: request.query.merchant_order_id
-	})
+app.get('/feedback', function(req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
 });
 
 app.listen(8080, () => {
