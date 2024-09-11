cebe0b818685eb1b50837d61394db445aaa24b6e
juan beresiarte <juanberesiarte@gmail.com>
2024-09-11 17:11:12 -0300

    Modificando el archivo server.js que viene por defecto

---
+++ server/old_server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "<ACCESS_TOKEN>",
+});
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("../../client/html-js"));
+app.use(cors());
+app.get("/", function (req, res) {
+	res.status(200).sendFile("index.html");
+});
+
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+});
+
+app.listen(8080, () => {
+	console.log("The server is now running on Port 8080");
+});
