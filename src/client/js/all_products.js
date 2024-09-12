b513d1253f94107063e2a1177388de10dccbd554
juan beresiarte <juanberesiarte@gmail.com>
2024-09-11 21:33:27 -0300

    Mostrando productos en la pantalla

---
+++ src/client/js/all_products.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        title: "Old Prince Premium (Perro adulto) ..",
+        price: 6400,
+        image: "/img/productos/1.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Purina Excelent (Perro adulto)",
+        price: 5500,
+        image: "/img/productos/2.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Purina Pro Plan (Perro adulto)",
+        price: 3450,
+        image: "/img/productos/3.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Purina excelent (Perro adulto)",
+        price: 6900,
+        image: "/img/productos/4.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Old Prince (Gato adulto)",
+        price: 2440,
+        image: "/img/productos/5.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Wishkas (Gato adulto)",
+        price: 9000,
+        image: "/img/productos/6.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Agility (Perro adulto)",
+        price: 9000,
+        image: "/img/productos/7.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Pro Plan (Perro adulto)",
+        price: 9000,
+        image: "/img/productos/8.webp",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Therapy (Perro adulto)",
+        price: 9000,
+        image: "/img/productos/9.webp",
+        id: crypto.randomUUID()
+    },
+];
--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,1 +1,43 @@
-console.log("Hola mundo! :)");
+import all_products from "./all_products.js";
+
+const products_container = document.querySelector("#products");
+
+const mostrarProducto = (producto) => {
+
+    const fragment = document.createDocumentFragment();
+
+    const container_product = document.createElement("div");
+
+    const header = document.createElement("div");
+
+    const image = document.createElement("img");
+
+    const body = document.createElement("div");
+
+    const title = document.createElement("p");
+
+    const price = document.createElement("span");
+
+    image.src = producto.image;
+    title.innerText = producto.title;
+    price.innerText = `$ ${producto.price}`;
+
+    header.appendChild(image);
+    body.appendChild(title);
+    body.appendChild(price);
+
+    const buy_button = document.createElement("button");
+    buy_button.innerText = "Comprar"
+
+    body.appendChild(buy_button);
+
+    container_product.appendChild(header);
+    container_product.appendChild(body);
+
+    fragment.appendChild(container_product);
+
+    products_container.appendChild(fragment);
+
+}
+
+all_products.forEach(producto => mostrarProducto(producto));
