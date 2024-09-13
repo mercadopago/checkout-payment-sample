657793ad47da59e821b3d923d411cb0cd30a4630
juan beresiarte <juanberesiarte@gmail.com>
2024-09-13 00:27:46 -0300

    Remove route /buy

--- src/main.js
+++ src/main.js
@@ -3,16 +3,11 @@ import mp_rutas from "./ruta_mercado_pago.js";
 
 app.use("/mp", mp_rutas);
 
-app.get("/ok", (req, res) => {
+app.get("/ok", (_req, res) => {
     return res.json({
         message: "El servidor esta andando! :D"
     });
 });
-
-app.post("/buy", (req, res) => {
-    console.log(req.body);
-    return res.status(200);
-})
 
 app.listen(app.get("port"), () => {
     console.log("Server http encendido! 😎");
