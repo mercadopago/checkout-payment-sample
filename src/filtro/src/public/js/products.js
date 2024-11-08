12f4175913b912f21b6b3846675a5a22b0995b39
AVBalmaceda <aadriveross@gmail.com>
2024-11-08 15:13:27 -0300

    Agrego modelo de filtro

---
+++ src/filtro/src/public/js/products.js
@@ -0,0 +1,66 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Old Prince Premium (Perro cachorro)",
+        price: 6400,
+        quanty: 1,
+        img: "/src/public/img/productos/1.webp"
+    },
+    {
+        id: 2,
+        productName: "Purina Excelent (Gato cachorro)",
+        price: 5500,
+        quanty: 1,
+        img: "/src/public/img/productos/2.webp"
+    },
+    {
+        id: 3,
+        productName: "Purina Pro Plan (Perro adulto)",
+        price: 3450,
+        quanty: 1,
+        img: "/src/public/img/productos/3.webp"
+    },
+    {
+        id: 4,
+        productName: "Purina Excelent (Perro cachorro)",
+        price: 6900,
+        quanty: 1,
+        img: "/src/public/img/productos/4.webp"
+    },
+    {
+        id: 5,
+        productName: "Old Prince (Gato adulto)",
+        price: 2440,
+        quanty: 1,
+        img: "/src/public/img/productos/5.webp"
+    },
+    {
+        id: 6,
+        productName: "Wishkas (Gato adulto)",
+        price: 9000,
+        quanty: 1,
+        img: "/src/public/img/productos/6.webp"
+    },
+    {
+        id: 7,
+        productName: "Agility (Perro adulto)",
+        price: 9000,
+        quanty: 1,
+        img: "/src/public/img/productos/7.webp"
+    },
+    {
+        id: 8,
+        productName: "Pro Plan (Perro adulto)",
+        price: 9000,
+        quanty: 1,
+        img: "/src/public/img/productos/8.webp"
+    },
+    {
+        id: 9,
+        productName: "Therapy (Perro adulto)",
+        price: 9000,
+        quanty: 1,
+        img: "/src/public/img/productos/9.webp"
+    }
+
+];
