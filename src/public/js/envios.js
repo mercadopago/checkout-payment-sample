9af098849f749f2fc42fa4bf39b25ec9934a034c
juan beresiarte <juanberesiarte@gmail.com>
2024-11-08 13:21:58 -0300

    Usando api para obtener los codigos postales

--- src/public/js/envios.js
+++ src/public/js/envios.js
@@ -1,19 +1,18 @@
 // Función para calcular el costo de envío basado en el código postal
-function calcularCostoEnvio(codigoPostal) {
-    const costosEnvio = {
-        "5521": 3200,
-        "5501": 3300,
-        "5500": 3400,
-        "5539": 3550,
-        "5507": 3700,
-        "5600": 3600,
-        "5613": 3450,
-        "5620": 2450,
-        "5515": 3000,
-    };
+async function calcularCostoEnvio(codigoPostal) {
+    const res = await fetch(`/cp/${codigoPostal}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    const data = await res.json();
+
+    if (res.ok) return data.price;
 
     // Si el código postal está en la lista, devuelve el costo, si no, da un costo estándar
-    return costosEnvio[codigoPostal] || "Código postal no encontrado. Aun no tenemos envios a tu zona";
+    return "Código postal no encontrado. Aun no tenemos envios a tu zona";
 }
 
 // Función para validar el código postal
@@ -24,7 +23,7 @@ function validarCodigoPostal(codigoPostal) {
 }
 
 // Función principal que usa el sistema de envío
-function procesarEnvio() {
+async function procesarEnvio() {
     //Se obtiene el valor del input de codigo postal
     const codigoPostalInput = document.getElementById("codigoPostal").value;
     // Referencias a los elementos donde se mostraran los mensajes de costo y error
@@ -39,7 +38,7 @@ function procesarEnvio() {
         // Llama a la funcion de validacion del codigo postal
         validarCodigoPostal(codigoPostalInput);
         // Si es valido se obtiene el costo para el codigo ingresado
-        const costo = calcularCostoEnvio(codigoPostalInput);
+        const costo = await calcularCostoEnvio(codigoPostalInput);
 
 
         if (typeof costo === "string") {
